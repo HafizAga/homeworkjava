bd8863fa28dce2be69209641bd8eb18d60d30707
HafiAga <ulguiorra2@gmail.com>
2023-03-31 17:16:14 +0300

    Add hm3

---
+++ work3/7.java
@@ -0,0 +1,29 @@
+//1. Пусть дан произвольный список целых чисел, удалить из него четные числа
+
+package ru.geekbrains;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Task001 {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            int val = rnd.nextInt(-100, 100);
+            list1.add(val);
+        }
+        System.out.printf("Первоначальный список %s\n", list1);
+        int i = 0;
+        while (i < list1.size()) {
+            if (list1.get(i) % 2 == 0) {
+                list1.remove(i);
+            }
+            else {
+                i++;
+            }
+        }
+        System.out.printf("Список с удаленными четными числами %s", list1);
+    }
+}
+
---
+++ work3/8.java
@@ -0,0 +1,44 @@
+// 2. Задан целочисленный список ArrayList. Найти минимальное,
+// максимальное и среднее из этого списка.
+
+package ru.geekbrains;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import static java.lang.Math.round;
+import static java.util.Collections.max;
+import static java.util.Collections.min;
+
+public class Task002 {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            int val = rnd.nextInt(-100, 100);
+            list1.add(val);
+        }
+        System.out.printf("Первоначальный список %s\n", list1);
+        int max = max(list1);
+        int min = min(list1);
+        int maxItem = list1.get(0);
+        int minItem = list1.get(0);
+        int sumItems = 0;
+        for (int item : list1) {
+            if (item > maxItem) {
+                maxItem = item;
+            }
+            if (item < minItem) {
+                minItem = item;
+            }
+            sumItems += item;
+        }
+        float average = (float)sumItems/list1.size();
+        System.out.printf("Максимальный элемент при помощи max %s\n", max);
+        System.out.printf("Максимальный элемент %s\n", maxItem);
+        System.out.printf("Минимальный элемент при помощи min %s\n", min);
+        System.out.printf("Минимальный элемент %s\n", minItem);
+        System.out.printf("Сумма элементов %s\n", sumItems);
+        System.out.printf("Среднее арифметическое %s\n", average);
+    }
+}
